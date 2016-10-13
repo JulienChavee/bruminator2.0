6280e89469b72a01b71550dbba11c7435da73ac3
Jujuwar <julien.chavee@gmail.com>
2016-10-13 15:40:09 +0200

    - Ajout de la gallerie (musée)

---
+++ src/MainBundle/Controller/GallerieController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace MainBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class GallerieController extends Controller
+{
+    /**
+     * @Route("/gallerie", name="gallerie")
+     */
+    public function indexAction()
+    {
+        $res = json_decode( file_get_contents( 'https://concours.bruminator.eu/results.php' ), true );
+
+        return $this->render( 'MainBundle:Gallerie:index.html.twig', array( 'res' => $res ) );
+    }
+}
