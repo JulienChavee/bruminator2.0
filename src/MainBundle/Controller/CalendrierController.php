8daebe7e7a36c2f218e7268353ecee4f1dfca32e
Jujuwar <julien.chavee@gmail.com>
2016-10-28 17:55:48 +0200

    - Début de l'ajout de la page pour l'arbre des phases finales

--- src/MainBundle/Controller/CalendrierController.php
+++ src/MainBundle/Controller/CalendrierController.php
@@ -18,4 +18,15 @@ class CalendrierController extends Controller
         $matchs = $em->getRepository( 'MatchBundle:Matchs' )->findByDate( (new \DateTime())->setTimestamp( mktime( 0, 0, 0, date( "m" ), 1, date( "Y" ) ) ), array( 'field' => 'date', 'type' => 'ASC') );
         return $this->render( 'MainBundle:Calendrier:index.html.twig', array( 'time' => $time, 'matchs' => $matchs ) );
     }
+
+    /**
+     * @Route("/arbre", name="arbre")
+     */
+    public function arbreAction() {
+        $em = $this->getDoctrine()->getManager();
+
+        $matchs = $em->getRepository( 'MatchBundle:Matchs' )->findMatchsPhasesFinales();
+
+        return $this->render( 'MainBundle:Calendrier:arbre.html.twig', array( 'matchs' => $matchs ) );
+    }
 }
