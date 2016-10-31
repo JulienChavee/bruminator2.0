a02cb4e5348ba56f002e0e609f5cc87b6bc56310
Jujuwar <julien.chavee@gmail.com>
2016-10-31 12:23:30 +0100

    - Ajout de la possibilité de changer de mois dans le calendrier

--- src/MainBundle/Controller/CalendrierController.php
+++ src/MainBundle/Controller/CalendrierController.php
@@ -4,6 +4,8 @@ namespace MainBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class CalendrierController extends Controller
 {
@@ -14,9 +16,10 @@ class CalendrierController extends Controller
     {
         $em = $this->getDoctrine()->getManager();
 
-        $time = new \DateTime();
-        $matchs = $em->getRepository( 'MatchBundle:Matchs' )->findByDate( (new \DateTime())->setTimestamp( mktime( 0, 0, 0, date( "m" ), 1, date( "Y" ) ) ), array( 'field' => 'date', 'type' => 'ASC') );
-        return $this->render( 'MainBundle:Calendrier:index.html.twig', array( 'time' => $time, 'matchs' => $matchs ) );
+        $timeNow = new \DateTime();
+        $time = (new \DateTime())->setTimestamp( mktime( 0, 0, 0, date( "m" ), 1, date( "Y" ) ) );
+        $matchs = $em->getRepository( 'MatchBundle:Matchs' )->findByDate( $time, array( 'field' => 'date', 'type' => 'ASC') );
+        return $this->render( 'MainBundle:Calendrier:index.html.twig', array( 'time' => $time, 'matchs' => $matchs, 'timeNow' => $timeNow ) );
     }
 
     /**
@@ -28,5 +31,30 @@ class CalendrierController extends Controller
         $matchs = $em->getRepository( 'MatchBundle:Matchs' )->findMatchsPhasesFinales();
 
         return $this->render( 'MainBundle:Calendrier:arbre.html.twig', array( 'matchs' => $matchs ) );
+    }
+
+    /**
+     * @Route("/calendrier/ajax/get", name="calendrier_ajax_get")
+     */
+    public function ajaxGetCalendar( Request $request ) {
+        if( $request->isXmlHttpRequest() ) {
+            try {
+                $em = $this->getDoctrine()->getManager();
+
+                $time = (new \DateTime())->setTimestamp( mktime( 0, 0, 0, $request->get( 'month' ), 1, $request->get( 'year' ) ) );
+                $timeNow = new \DateTime();
+                $matchs = $em->getRepository( 'MatchBundle:Matchs' )->findByDate( $time, array( 'field' => 'date', 'type' => 'ASC') );
+
+                $response = new Response( json_encode( array( 'status' => 'ok', 'return' => $this->render( 'MainBundle:Calendrier:calendarGrid.html.twig', array( 'matchs' => $matchs, 'time' => $time, 'timeNow' => $timeNow ) )->getContent() ) ) );
+            }
+            catch( \Exception $e ) {
+                $response = new Response( json_encode( array( 'status' => 'ko', 'message' => 'Une erreur inconnue s\'est produite', 'debug' => $e->getMessage() ) ) );
+            }
+            $response->headers->set( 'Content-Type', 'application/json' );
+            return $response;
+        }
+        $response = new Response( json_encode( array( 'status' => 'ko', 'message' => 'Accès non autorisé', 'debug' => 'Bad request' ) ) );
+        $response->headers->set( 'Content-Type', 'application/json') ;
+        return $response;
     }
 }
