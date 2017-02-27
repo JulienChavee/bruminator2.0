faae7cb4045797536eb2467c296d91c643bedcb0
Jujuwar <julien.chavee@gmail.com>
2017-02-27 21:09:11 +0100

    - Fix #111

--- src/MainBundle/Controller/NewsController.php
+++ src/MainBundle/Controller/NewsController.php
@@ -15,9 +15,15 @@ class NewsController extends Controller
         $em = $this->getDoctrine()->getManager();
         $news = $em->getRepository( 'MainBundle:News' )->findOneBy( array( 'id' => $id ) );
 
-        if( $this->get( 'cocur_slugify' )->slugify( $news->getTitle() ) == $slugNews )
-            return $this->render( 'MainBundle:News:index.html.twig', array( 'new' => $news ) );
-        else
-            return $this->redirectToRoute( 'news_view', array( 'id' => $news->getId(), 'slugNews' => $this->get( 'cocur_slugify' )->slugify( $news->getTitle() ) ) );
+        if( $news && ( $news->getDate() <= new \DateTime( 'now' ) || $this->isGranted( 'ROLE_ADMIN' ) ) ) {
+            if( $this->get( 'cocur_slugify' )->slugify( $news->getTitle() ) == $slugNews )
+                return $this->render('MainBundle:News:index.html.twig', array('new' => $news));
+            else
+                return $this->redirectToRoute( 'news_view', array( 'id' => $news->getId(), 'slugNews' => $this->get( 'cocur_slugify' )->slugify( $news->getTitle() ) ) );
+        } else {
+            $this->addFlash( 'danger', 'La news n\'existe pas' );
+
+            return $this->redirectToRoute( 'homepage' );
+        }
     }
 }
